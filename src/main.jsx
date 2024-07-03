3f4787f8088a5eeed1e1a7433cadfbfc1e5e807b
Guilherme Carvalho <carvalho_guilhermegomes@hotmail.com>
2024-07-02 21:45:54 -0300

    creat component form in SignIn

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Details } from "./pages/Details";
+// import { Details } from "./pages/Details";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
 import GlobalStyled from "./styles/global";
 import { Home } from "./pages/Home";
+import { SignIn } from "./components/Signin";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyled />
-      <Details />
+      <SignIn />
       <Home />
     </ThemeProvider>
   </React.StrictMode>
