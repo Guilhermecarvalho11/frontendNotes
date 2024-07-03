a84994f1201a215f11b2a44388b502dbd1fb9fa2
Guilherme Carvalho <carvalho_guilhermegomes@hotmail.com>
2024-07-03 14:55:03 -0300

    add backgroundImg in the form

--- src/components/Signin/index.jsx
+++ src/components/Signin/index.jsx
@@ -1,9 +1,10 @@
 import { FiMail, FiLock } from "react-icons/fi";
+import img from "../../assets/background.png";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
-import { Container, Form } from "./styles";
+import { Container, Form, Background } from "./styles";
 
 export function SignIn() {
   return (
@@ -22,6 +23,7 @@ export function SignIn() {
 
         <a href="#">Criar conta</a>
       </Form>
+      <Background />
     </Container>
   );
 }
