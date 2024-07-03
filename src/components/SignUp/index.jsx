1c3d5a272d90a56884aef0c18521795d7e942fd7
Guilherme Carvalho <carvalho_guilhermegomes@hotmail.com>
2024-07-03 15:15:55 -0300

    create desktop componente SingUp

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -1,28 +1,30 @@
-import { FiMail, FiLock } from "react-icons/fi";
+import { FiMail, FiLock, FiUser } from "react-icons/fi";
 
-import { Input } from "../../components/Input";
-import { Button } from "../../components/Button";
+import { Input } from "../Input";
+import { Button } from "../Button";
 
 import { Container, Form, Background } from "./styles";
 
-export function SignIn() {
+export function SignUp() {
   return (
     <Container>
+      <Background />
+
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
-        <h2>Faça seu login</h2>
+        <h2>Crie Sua conta</h2>
+        <Input placeholder="Nome" type="text" icon={FiUser} />
 
         <Input placeholder="E-mail" type="text" icon={FiMail} />
 
         <Input placeholder="Senha" type="password" icon={FiLock} />
 
-        <Button title="Entrar" />
+        <Button title="Cadastrar" />
 
-        <a href="#">Criar conta</a>
+        <a href="#">Voltar para o Login</a>
       </Form>
-      <Background />
     </Container>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,13 @@ import theme from "./styles/theme";
 import GlobalStyled from "./styles/global";
 import { Home } from "./pages/Home";
 import { SignIn } from "./components/Signin";
+import { SignUp } from "./components/SignUp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyled />
+      <SignUp />
       <SignIn />
       <Home />
     </ThemeProvider>
